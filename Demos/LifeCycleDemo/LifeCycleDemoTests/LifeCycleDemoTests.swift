5cc492178e32bc13b3bd87f8523bbc5e30fc009a
王瑞吉 <wangruiji@bytedance.com>
2018-11-29 19:14:48 +0800

    1. 文件夹改名 2. 移动新文章的位置 3. 上传新文章的 Demo
    Former-commit-id: f2d8fbd21714f4909e7763b0da853757d484f73c

---
+++ Demos/LifeCycleDemo/LifeCycleDemoTests/LifeCycleDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  LifeCycleDemoTests.swift
+//  LifeCycleDemoTests
+//
+//  Created by 王瑞吉 on 2018/11/19.
+//  Copyright © 2018 王瑞吉. All rights reserved.
+//
+
+import XCTest
+@testable import LifeCycleDemo
+
+class LifeCycleDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
